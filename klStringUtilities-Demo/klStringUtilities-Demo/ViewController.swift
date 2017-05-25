f9a6bae65ca7ce5e6fa23c0d54873d9b0019adaa
Ken Laws <ken@kenlaws.tv>
2017-05-25 10:22:01 -0700

    Reworking layout.

---
+++ klStringUtilities-Demo/klStringUtilities-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  klStringUtilities-Demo
+//
+//  Created by Ken Laws on 5/25/17.
+//  Copyright © 2017 dela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
