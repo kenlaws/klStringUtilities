bd2f54c3f2ca438036fbb30df236a85a1eb71e54
Ken Laws <ken@kenlaws.tv>
2017-05-26 13:51:10 -0700

    Adding back a couple of utf16 versions.

--- klStringUtilities/StringUtilities.swift
+++ klStringUtilities/StringUtilities.swift
@@ -35,6 +35,11 @@ public class StringUtilities {
 public extension String {
 	
 	var len: Int {
+		return self.characters.count
+	}
+
+	
+	var charLen: Int {
 		var count = 0
 		self.enumerateSubstrings(in: self.startIndex ..< self.endIndex, options: .byComposedCharacterSequences) { (_, _, _, _) in
 			count += 1
@@ -64,6 +69,17 @@ public extension String {
 
 	
 	func rangeFromNSRange(nsRange : NSRange) -> Range<String.Index>? {
+		guard
+			let from16 = utf16.index(utf16.startIndex, offsetBy: nsRange.location, limitedBy: utf16.endIndex),
+			let to16 = utf16.index(utf16.startIndex, offsetBy: nsRange.location + nsRange.length, limitedBy: utf16.endIndex),
+			let from = from16.samePosition(in: self),
+			let to = to16.samePosition(in: self)
+			else { return nil }
+		return from ..< to
+	}
+
+
+	func charRangeFromNSRange(nsRange : NSRange) -> Range<String.Index>? {
 		var charCount = 0
 		var fromRng:Range<String.Index>?
 		var toRng:Range<String.Index>?
